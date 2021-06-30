192c1e8fe53daa0c5b1d294df10d77abef4df9d1
Gabaunzito <gabrielpmachado.contato@gmail.com>
2021-06-30 20:11:17 -0300

    Criação BinarySearch.

--- ProjetoOrdenacao/src/Entidades/Pesquisas/BinarySearch.java
+++ ProjetoOrdenacao/src/Entidades/Pesquisas/BinarySearch.java
@@ -1,6 +1,38 @@
 package Entidades.Pesquisas;
 
-public class BinarySearch extends Research
-{
+public class BinarySearch extends Research {
+
+    @Override
+    public ResultadoPesquisa Pesquisa(long[] entrada, long valor) {
+
+        ResultadoPesquisa resultado = new ResultadoPesquisa();
+
+        long tempoInicial = System.currentTimeMillis();
+
+        int retorno = pesquisaBinaria(entrada,0,entrada.length-1,valor);
+
+        long tempoFinal = System.currentTimeMillis();
+
+        resultado.setTempo("Executado em = " + (tempoFinal - tempoInicial) + " ms");
+
+        resultado.setResultado(retorno);
+
+        return resultado;
+    }
+
+
+    private int pesquisaBinaria(long[] vetor, int esquerda, int direita, long valor) {
+        if(direita >= esquerda) {
+            int indice = (esquerda + (direita-esquerda)/2);
+            if(vetor[indice]==valor) {
+                return indice;
+            }
+            if(vetor[indice] > valor) {
+                return pesquisaBinaria(vetor,esquerda,indice-1,valor);
+            }
+            return pesquisaBinaria(vetor, indice+1, direita, valor);
+        }
+        return-1;
+    }
 
 }
